3d6c11880da1f5242a334acb008d80e2159d6040
Sarpong128 <sparrowjaqx@gmail.com>
2023-02-19 13:37:45 -0800

    Print Comb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Print all possible different combinations of 3 digits.
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 48; x < 58; x++)
+	{
+		for (y = 49; y < 58; y++)
+		{
+			for (z = 50; z < 58; z++)
+			{
+				if (y > x && z > y)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x != 55 || y != 56 || z != 57)
+						A
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
