d18b00176a220fc496286de1e7e7e84ebe3b4890
Sarpong128 <sparrowjaqx@gmail.com>
2023-04-10 19:58:07 -0700

    Solution to Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a number in binary
+ *
+ * @n: number to be printted
+ *
+ * Return: Nothing.
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int div;
+	char m;
+
+	div = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	m = 0;
+
+	while (div != 0)
+	{
+		if (n & div)
+		{
+			m = 1;
+			_putchar('1');
+		}
+		else if (m)
+		{
+			_putchar('0');
+		}
+		div >>=	1;
+	}
+
+	if (!m)
+	{
+		_putchar('0');
+	}
+}
