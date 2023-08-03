d67a68ee4f68d7b0b59445f436fb70fd1f669f44
Sarpong128 <sparrowjaqx@gmail.com>
2023-08-02 18:23:08 -0700

    second

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,4 @@
-i#include "lists.h"
+#include "lists.h"
 
 /**
  * print_listint - prints all elements of a linked list
@@ -12,7 +12,7 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-
+	
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
